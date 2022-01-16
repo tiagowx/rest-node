5df6292e5939865e0b1724eb6f1dccd97d41eaf6
wxtiago <tiagobatista.tec@gmail.com>
2022-01-16 14:58:52 -0300

    Rota post e conversores de dados

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import express, { Request, Response, NextFunction } from 'express';
 import usersRoute from './routes/user.route';
 
-
+// Instanciando Aplicação
 const app = express();
 
+// Configurando Aplicação
+app.use(express.json()); // Converte requisições em Json
+app.use(express.urlencoded({ extended: true })); // Habilita querystring na conversão de dados passados na Url
+
+// Configurando Rotas
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ status: 'online' });
 });
 
-app.listen(3000, () =>{
-  const url= "http://localhost/status";
-  console.log(`App exec in port: 3000 ${url.link}`);
+// Inicializando Aplicação
+
+app.listen(3000, () => {
+  const url = "http://localhost:3000";
+  console.log(`Aplicação executando no caminho: ${url}`);
 
 });
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -7,10 +7,15 @@ const usersRoute = Router();
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
   const users = [{ userName: 'Tiago' }];
   res.status(StatusCodes.OK).send(users);
-})
+});
 usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
   const uuid = req.params.uuid;
   res.status(StatusCodes.OK).send({ uuid });
-})
+});
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+  const newUser = req.body;
+  res.status(StatusCodes.CREATED).send({ newUser });
+});
 
 export default usersRoute;
