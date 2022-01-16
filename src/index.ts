56f2c8e9c0e23ce0e842e4faaebf8c072cc24cee
wxtiago <tiagobatista.tec@gmail.com>
2022-01-16 12:37:22 -0300

    Configurado servidor express e tipagem typescripts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Executei!')
+import { Console } from 'console';
+import express, { Request, Response, NextFunction } from 'express';
+
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ status: 'online' });
+});
+
+app.listen(3000, () =>{
+  console.log('App exec in port: 3000');
+
+});
