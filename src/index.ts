299127b39974aa45716f622a990ae273b3ac8456
wxtiago <tiagobatista.tec@gmail.com>
2022-01-16 14:26:26 -0300

    Usando Statuscode

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import { Console } from 'console';
 import express, { Request, Response, NextFunction } from 'express';
+import usersRoute from './routes/user.route';
 
 
 const app = express();
+
+app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ status: 'online' });
 });
 
 app.listen(3000, () =>{
-  console.log('App exec in port: 3000');
+  const url= "http://localhost/status";
+  console.log(`App exec in port: 3000 ${url.link}`);
 
 });
---
+++ src/routes/user.route.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { StatusCodes } from "http-status-codes";
+
+
+const usersRoute = Router();
+
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
+  const users = [{ userName: 'Tiago' }];
+  res.status(StatusCodes.OK).send(users);
+})
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  res.status(StatusCodes.OK).send({ uuid });
+})
+
+export default usersRoute;
