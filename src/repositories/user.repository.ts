539aec04bd9b41ecba359816fee474453cd753ac
wxtiago <tiagobatista.tec@gmail.com>
2022-01-17 13:27:06 -0300

    Criados repositório de usuário e requisição padrão

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,8 +1,16 @@
+import db from "../db";
 import User from "../models/user.model";
 
 class UserRepository {
-  findAllUsers(): User[] {
-    return [];
+  async findAllUsers(): Promise<User[]> {
+    const query = `
+      SELECT uuid, username 
+      FROM app_auth_user
+    `;
+    const result = await db.query<User>(query);
+    const rows = result.rows;
+
+    return rows || [];
   }
 }
 
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { StatusCodes } from "http-status-codes";
+import userRepository from "../repositories/user.repository";
 
 const usersRoute = Router();
 
-usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
-  const users = [{ userName: 'Tiago' }];
+usersRoute.get('/users', async (req: Request, res: Response, next: NextFunction) => {
+  const users = await userRepository.findAllUsers();
   res.status(StatusCodes.OK).send(users);
 });
 
