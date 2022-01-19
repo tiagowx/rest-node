5aaab8b9b7e81df94b49bcc5efdbdb729439e83a
wxtiago <tiagobatista.tec@gmail.com>
2022-01-19 00:14:01 -0300

    Gerando JWT

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { StatusCodes } from "http-status-codes";
+import JWT from "jsonwebtoken";
 import ForbiddenError from "../models/errors/forbidden.error.model";
 import userRepository from "../repositories/user.repository";
 
@@ -20,17 +22,29 @@ authorizationRoute.post('/token', async (req: Request, res: Response, next: Next
       throw new ForbiddenError('Autenticação inválida');
     }
 
-    const tokenContent = Buffer.from(token,'base64').toString('utf-8');
+    const tokenContent = Buffer.from(token, 'base64').toString('utf-8');
 
     const [username, password] = tokenContent.split(':');
 
-    if(!username || !password){
+    if (!username || !password) {
       throw new ForbiddenError('Credenciais não preenchidas');
     }
 
     const user = await userRepository.findByUserAuth(username, password);
 
+    if (!user) {
+      throw new ForbiddenError('Usuário ou senha inválidos')
+    }
+
     console.log(user);
+
+    const jwtPayload = { username: user.username };
+    const jwtOptions = { subject: user?.uuid };
+    const secretKey = 'my_secret_key';
+
+    const jwt = JWT.sign(jwtPayload, secretKey, jwtOptions);
+
+    res.status(StatusCodes.OK).json({ token: jwt });
 
   } catch (error) {
     next(error);
