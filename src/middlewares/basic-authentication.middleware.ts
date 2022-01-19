14855e83cd9e24e005e7b7fbf54d3facb1f00c4f
wxtiago <tiagobatista.tec@gmail.com>
2022-01-19 00:36:38 -0300

    Configurado Middlewarede de Autenticação.

---
+++ src/middlewares/basic-authentication.middleware.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+import ForbiddenError from "../models/errors/forbidden.error.model";
+import userRepository from "../repositories/user.repository";
+
+async function basicAuthenticationMiddleware(req: Request,res: Response, next: NextFunction) {
+try {
+  const authorizationHeaders = req.headers['authorization'];
+
+  if (!authorizationHeaders) {
+    throw new ForbiddenError('Credenciais não informadas');
+  }
+  // Basic dGlhZ286MTIzNDU2
+  const [authenticationTypm, token] = authorizationHeaders.split(' ');
+
+
+  if (authenticationTypm !== 'Basic' || !token) {
+    throw new ForbiddenError('Autenticação inválida');
+  }
+
+  const tokenContent = Buffer.from(token, 'base64').toString('utf-8');
+
+  const [username, password] = tokenContent.split(':');
+
+  if (!username || !password) {
+    throw new ForbiddenError('Credenciais não preenchidas');
+  }
+
+  const user = await userRepository.findByUserAuth(username, password);
+
+  if (!user) {
+    throw new ForbiddenError('Usuário ou senha inválidos')
+  }
+
+  req.user = user;
+} catch (error) {
+  
+}
+}
+
+export default basicAuthenticationMiddleware;
--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,42 +1,19 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { StatusCodes } from "http-status-codes";
 import JWT from "jsonwebtoken";
+import basicAuthenticationMiddleware from "../middlewares/basic-authentication.middleware";
 import ForbiddenError from "../models/errors/forbidden.error.model";
-import userRepository from "../repositories/user.repository";
 
 const authorizationRoute = Router();
 
-authorizationRoute.post('/token', async (req: Request, res: Response, next: NextFunction) => {
+authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Request, res: Response, next: NextFunction) => {
 
   try {
-    const authorizationHeaders = req.headers['authorization'];
-
-    if (!authorizationHeaders) {
-      throw new ForbiddenError('Credenciais não informadas');
-    }
-    // Basic dGlhZ286MTIzNDU2
-    const [authenticationTypm, token] = authorizationHeaders.split(' ');
-
-
-    if (authenticationTypm !== 'Basic' || !token) {
-      throw new ForbiddenError('Autenticação inválida');
-    }
-
-    const tokenContent = Buffer.from(token, 'base64').toString('utf-8');
-
-    const [username, password] = tokenContent.split(':');
-
-    if (!username || !password) {
-      throw new ForbiddenError('Credenciais não preenchidas');
-    }
-
-    const user = await userRepository.findByUserAuth(username, password);
+    const user = req.user;
 
     if (!user) {
-      throw new ForbiddenError('Usuário ou senha inválidos')
+      throw new ForbiddenError('Usuário não informado')
     }
-
-    console.log(user);
 
     const jwtPayload = { username: user.username };
     const jwtOptions = { subject: user?.uuid };
@@ -45,6 +22,8 @@ authorizationRoute.post('/token', async (req: Request, res: Response, next: Next
     const jwt = JWT.sign(jwtPayload, secretKey, jwtOptions);
 
     res.status(StatusCodes.OK).json({ token: jwt });
+
+    next();
 
   } catch (error) {
     next(error);
